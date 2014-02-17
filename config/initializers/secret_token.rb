88b41d378dadf71635d8a12b58c7899fc439a5f5
Scott Lieberman <scottklieberman@gmail.com>
2014-02-17 11:24:48 -0600

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sauced::Application.config.secret_key_base = 'b5c2c33d2cfc04a9321f001feb34bbd124f047bead96aa8d8c3231e8f8f570c00c6e002ea7293041e6a645f853541e372cd5e4086c7446aa8f6238542744711e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sauced::Application.config.session_store :cookie_store, key: '_sauced_session'
